8cd3bfe2f054341bd7d7d45cd93ff31b90a0256b
Cristina López-Goicochea Juárez <cjuarez@atsistemas.com>
2020-02-24 09:35:56 +0100

    Initial Commit

---
+++ src/main/java/com/example/pages/BasePage.java
@@ -0,0 +1,35 @@
+package com.example.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+
+public class BasePage {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public BasePage (WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver,20);
+    }
+
+    public String readText(By elementLocation) {
+        return driver.findElement(elementLocation).getText();
+    }
+
+    public void writeText(By elementLocation, String text) {
+        driver.findElement(elementLocation).sendKeys(text);
+    }
+
+    public WebElement getElement(By element) {
+        return driver.findElement(element);
+    }
+
+}
