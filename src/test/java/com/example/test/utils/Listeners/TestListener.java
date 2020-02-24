7dc889a93a44a84723e52773947a66db0759c3f5
Cristina López-Goicochea Juárez <cjuarez@atsistemas.com>
2020-02-24 10:56:31 +0100

    Initial Commit

--- src/test/java/com/example/test/utils/Listeners/TestListener.java
+++ src/test/java/com/example/test/utils/Listeners/TestListener.java
@@ -1,9 +1,6 @@
 package com.example.test.utils.Listeners;
 
 import com.example.test.BaseTest;
-import com.example.test.utils.ExtentReports.ExtentManager;
-import com.example.test.utils.ExtentReports.ExtentTestManager;
-import com.relevantcodes.extentreports.LogStatus;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -19,19 +16,16 @@ public class TestListener extends BaseTest implements ITestListener {
         return iTestResult.getMethod().getConstructorOrMethod().getName();
     }
 
-    //Text attachments for Allure
     @Attachment(value = "Page screenshot", type = "image/png")
     public byte[] saveScreenshotPNG (WebDriver driver) {
         return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
     }
 
-    //Text attachments for Allure
     @Attachment(value = "{0}", type = "text/plain")
     public static String saveTextLog (String message) {
         return message;
     }
 
-    //HTML attachments for Allure
     @Attachment(value = "{0}", type = "text/html")
     public static String attachHtml(String html) {
         return html;
@@ -44,57 +38,32 @@ public class TestListener extends BaseTest implements ITestListener {
 
     @Override
     public void onFinish(ITestContext iTestContext) {
-        //Do tier down operations for extentreports reporting!
-        ExtentTestManager.endTest();
-        ExtentManager.getReporter().flush();
     }
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
-        //Start operation for extentreports.
-        ExtentTestManager.startTest(iTestResult.getMethod().getMethodName(),"");
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        //Extentreports log operation for passed tests.
-        ExtentTestManager.getTest().log(LogStatus.PASS, "Test passed");
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        //Get driver from BaseTest and assign to local webdriver variable.
         Object testClass = iTestResult.getInstance();
         WebDriver driver = ((BaseTest) testClass).getDriver();
 
-        //Allure ScreenShotRobot and SaveTestLog
         if (driver instanceof WebDriver) {
-            System.out.println("Screenshot captured for test case:" + getTestMethodName(iTestResult));
             saveScreenshotPNG(driver);
         }
-
-        //Save a log on allure.
-        saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");
-
-        //Take base64Screenshot screenshot for extent reports
-        String base64Screenshot = "data:image/png;base64,"+((TakesScreenshot)driver).
-                getScreenshotAs(OutputType.BASE64);
-
-        //Extentreports log and screenshot operations for failed tests.
-        ExtentTestManager.getTest().log(LogStatus.FAIL,"Test Failed",
-                ExtentTestManager.getTest().addBase64ScreenShot(base64Screenshot));
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
-        System.out.println("I am in onTestSkipped method "+  getTestMethodName(iTestResult) + " skipped");
-        //Extentreports log operation for skipped tests.
-        ExtentTestManager.getTest().log(LogStatus.SKIP, "Test Skipped");
     }
 
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
-        System.out.println("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
     }
 
 }
